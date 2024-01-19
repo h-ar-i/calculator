40805ea48d465381d83c062ef3f910505fd21c5f
unknown <hk3700680@gmail.com>
2024-01-19 18:44:34 +0530

    initial commit

---
+++ cal.js
@@ -0,0 +1,18 @@
+//display content
+const displaycontent=(content)=>{
+    //result is id of input tag
+    result.value += content
+}
+
+const clearscreen = ()=>{
+    result.value=""
+}
+const finaloutput  =()=>{
+    try{result.value = eval(result.value)}
+    catch{
+        result.value="ErroR"
+    }
+}
+const removeitem=()=>{
+    result.value=result.value.slice(0,-1)
+}
